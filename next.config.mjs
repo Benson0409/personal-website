8be2ac6e699e276d19cb350c78a98499a1b9b137
Benson0409 <benson5508@gmail.com>
2025-11-20 22:14:55 +0800

    修改網頁推送問題７
    改用vercel 進行部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,13 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 
-const isDev = process.env.NODE_ENV !== 'production'; 
-const repoName = '/personal-website'; 
-
 const nextConfig = {
-  // basePath 保持不變 (以 / 開頭)
-  basePath: isDev ? undefined : repoName, 
   
-  // 【關鍵最終修正】將 assetPrefix 設為空字串 '' 
-  // 這將會確保資源連結不帶有任何前綴，從而避免與 basePath 產生雙重前綴。
-  assetPrefix: isDev ? undefined : '', 
-
-  // 設置輸出模式為靜態
-  output: 'export', 
+  output: 'standalone', 
+  
   reactStrictMode: true,
   images: {
-    unoptimized: true
+    
   },
 };
 
